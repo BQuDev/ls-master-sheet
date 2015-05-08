7fd141f0781f601b8360a05816b229a2e95809c7
BQumobile <bqu.mobile.dev@gmail.com>
2015-05-08 13:36:32 +0530

    index

--- resources/views/admissions/index.blade.php
+++ resources/views/admissions/index.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.main');
 @section('content')
-Application Saved Successfully.
+
+
+<div class="alert alert-success">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <i class="fa fa-ok-sign"></i><strong>Well done!</strong>Application Saved Successfully..
+                  </div>
+
+                <br><br>
+                  <a href="{!! URL::to('/') !!}" class="btn btn-s-md btn-primary">Primary</a>
 @stop
