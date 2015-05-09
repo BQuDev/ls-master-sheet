6a94d4d2d7cbb3b453f583fa8c907516f4eb7846
BQumobile <bqu.mobile.dev@gmail.com>
2015-05-09 23:29:11 +0530

    user seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,16 +5,56 @@ use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        // $this->call('UserTableSeeder');
+
+        try
+        {
+            // Create the group
+            $group = Sentry::createGroup(array(
+                'name'        => 'Administrator',
+                'permissions' => array(
+                    'admin' => 1,
+                    'users' => 1,
+                ),
+            ));
+        }
+        catch (Cartalyst\Sentry\Groups\NameRequiredException $e)
+        {
+            echo 'Name field is required';
+        }
+        catch (Cartalyst\Sentry\Groups\GroupExistsException $e)
+        {
+            echo 'Group already exists';
+        }
+
+        try
+        {
+            // Create the user
+            $user = Sentry::createUser(array(
+                'email'     => 'admin@bqu.com',
+                'password'  => '123',
+                'activated' => true,
+            ));
+
+            // Find the group using the group id
+            $adminGroup = Sentry::findGroupById(1);
+
+            // Assign the group to the user
+            $user->addGroup($adminGroup);
+        }
+        catch (Cartalyst\Sentry\Users\LoginRequiredException $e)
+        {
+            echo 'Login field is required.';
+        }
+    }
 
 }
