a234783eb2d4011718a29ab6b296fe9f884230d2
BQumobile <bqu.mobile.dev@gmail.com>
2015-05-14 08:20:41 +0530

    database

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'xxxx',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => '217.174.240.99',
+			'database'  => 'bqutest_test',
+			'username'  => 'todel',
+			'password'  => '2!jMj20k',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
