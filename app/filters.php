609faadad19d92fe7c1ef387938c2b6d9c6f89d2
BQumobile <bqu.mobile.dev@gmail.com>
2015-05-15 20:30:25 +0530

    ipset

--- app/filters.php
+++ app/filters.php
@@ -16,8 +16,23 @@ App::before(function($request)
 	//
 	if( !Request::secure() && App::environment() !== 'local')
 		{
-            Request::setTrustedProxies([$request->getClientIp()]);
+           // Request::setTrustedProxies([$request->getClientIp()]);
 		//$request->setTrustedProxies( [ $request->getClientIp() ] ); 
+		Request::setTrustedProxies(array(
+    '199.27.128.0/21',
+    '173.245.48.0/20',
+    '103.21.244.0/22',
+    '103.22.200.0/22',
+    '103.31.4.0/22',
+    '141.101.64.0/18',
+    '108.162.192.0/18',
+    '190.93.240.0/20',
+    '188.114.96.0/20',
+    '197.234.240.0/22',
+    '198.41.128.0/17',
+    '162.158.0.0/15',
+));
+
 		return Redirect::secure(Request::getRequestUri());
 		}
 });
