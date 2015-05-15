a51a71afb28449455fd93ed8b20bd205451766bb
BQumobile <bqu.mobile.dev@gmail.com>
2015-05-15 20:36:51 +0530

    https working

--- app/filters.php
+++ app/filters.php
@@ -12,31 +12,9 @@
 */
 
 App::before(function($request)
-{ Request::setTrustedProxies([$request->getClientIp()]);
-	//
-	/*
-	if( !Request::secure())
-		{
-           //
-		//$request->setTrustedProxies( [ $request->getClientIp() ] ); 
-		Request::setTrustedProxies(array(
-    '199.27.128.0/21',
-    '173.245.48.0/20',
-    '103.21.244.0/22',
-    '103.22.200.0/22',
-    '103.31.4.0/22',
-    '141.101.64.0/18',
-    '108.162.192.0/18',
-    '190.93.240.0/20',
-    '188.114.96.0/20',
-    '197.234.240.0/22',
-    '198.41.128.0/17',
-    '162.158.0.0/15',
-));
+{ 
+	Request::setTrustedProxies([$request->getClientIp()]);
 
-		return Redirect::secure(Request::getRequestUri());
-		}*/
-		
 		if( ! Request::secure())
     {
         return Redirect::secure(Request::path());
