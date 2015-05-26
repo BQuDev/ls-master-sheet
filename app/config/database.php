97bd01abd2a74730acdd75c845b065817a3b4294
BQumobile <bqu.mobile.dev@gmail.com>
2015-05-26 16:19:43 +0530

    misty-hurricane

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '217.174.240.99',
-			'database'  => 'bqutest_test',
-			'username'  => 'todel',
-			'password'  => '2!jMj20k',
+			'host'      => 'localhost',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => 'uhvvZYarSIGy5B8oWNJB',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
