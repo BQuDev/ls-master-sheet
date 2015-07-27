ecddca9a99d5e15f0be7ccce8a0f90ca30ad40b3
BQumobile <bqu.mobile.dev@gmail.com>
2015-07-27 16:49:23 +0530

    tt

--- app/filters.php
+++ app/filters.php
@@ -10,7 +10,7 @@
 | application. Here you may also register your custom route filters.
 |
 */
-/*
+
 App::before(function($request)
 { 
 	Request::setTrustedProxies([$request->getClientIp()]);
@@ -19,7 +19,7 @@ App::before(function($request)
     {
         return Redirect::secure(Request::path());
     }
-});*/
+});
 
 
 App::after(function($request, $response)
